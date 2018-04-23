a1136b982c8d44e60700d511c54b30271bfb57ab
jandoant <jandoant@gmail.com>
2018-04-23 16:41:00 +0200

    Adding StpSurface

---
+++ src/main/java/com/jandoant/stp_entities/StpSurface.java
@@ -0,0 +1,18 @@
+package com.jandoant.stp_entities;
+
+/**
+ * Klasse StpSurface
+ * Created by Jan Doant on 23.04.2018
+ */
+public class StpSurface extends StpGeometricRepresentationItem {
+
+    //Attribute
+
+    //Konstruktor
+    public StpSurface(int id, String name) {
+        super(id, name);
+    }
+
+    //Methoden
+
+}
